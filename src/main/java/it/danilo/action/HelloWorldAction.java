837c2559ab8109f07f796d9645a699d1f242b3e8
danilo <danilo.calzetta@gmail.com>
2023-03-15 00:17:18 +0100

    login form

--- src/main/java/it/danilo/action/HelloWorldAction.java
+++ src/main/java/it/danilo/action/HelloWorldAction.java
@@ -1,5 +1,7 @@
 package it.danilo.action;
 
+import java.util.Date;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
@@ -8,13 +10,17 @@ import org.apache.struts.action.ActionForm;
 import org.apache.struts.action.ActionForward;
 import org.apache.struts.action.ActionMapping;
 
+import it.danilo.form.HelloWorldForm;
+
 public class HelloWorldAction extends Action {
 	
 	//The ActionForm object will contain instance of our form bean i.e. HelloWorldForm.
 	//The execute method returns string "hello" as the ActionForward.
 	@Override
 	public ActionForward execute(ActionMapping mapping, ActionForm form, HttpServletRequest request, HttpServletResponse response) throws Exception {
-		
+		HelloWorldForm myForm = (HelloWorldForm) form;
+		Date date =  new Date();
+		myForm.setHelloProp("action class compila la propietà " + date.toString());
 		return mapping.findForward("hello");
 	}
 }
